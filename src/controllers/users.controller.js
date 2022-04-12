9411ac5d9769cc7f1f939013b59fa41440752e22
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2022-04-12 14:02:57 -0300

    Error Handlers Require: try-cactch

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,52 +3,75 @@ const config = require('config');
 const UserModel = require('../models/user.model');
 
 async function getUsers(req, res) {
-    const user = await UserModel.find()
-    res.send(user)
+    try {
+        const user = await UserModel.find()
+
+        return res.json(user)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 async function getByIdUser(req, res) {
-    const { id } = (req.params)
-    const user = await
-        UserModel.findById(id)
+    try {
+        const { id } = (req.params)
+        const user = await
+            UserModel.findById(id)
         if (!user) {
             return res.status(404).json({ message: "error: user does not exists" })
         }
-    return res.send(user)
+
+        return res.json(user)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 async function createUser(req, res) {
-    const { username, email, password } = req.body;
-    const user = new UserModel({ username, email, password });
-    await user.save();
-    res.send(user)
+    try {
+        const { username, email, password } = req.body;
+        const user = new UserModel({ username, email, password });
+        await user.save();
+
+        return res.json(user)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 async function deleteUser(req, res) {
-    const { id } = (req.params)
-    const user = await
-        UserModel.findByIdAndDelete(id)
-    
+    try {
+        const { id } = (req.params)
+        const user = await
+            UserModel.findByIdAndDelete(id)
+
         if (!user) {
             return res.status(404).json({ message: "error: user does not exists" })
         }
-    
-        return res.send('User deleted!')
+
+        return res.json('User deleted!')
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 };
 
 async function updateUser(req, res) {
-    const { id } = (req.params)
-    const { username, email, password } = req.body;
-    const user = await
-        UserModel.findByIdAndUpdate(id, { username, email, password }, { new: true })
-    
+    try {
+        const { id } = (req.params)
+        const { username, email, password } = req.body;
+        const user = await
+            UserModel.findByIdAndUpdate(id, { username, email, password }, { new: true })
+
         if (!user) {
             return res.status(404).json({ message: "error: user does not exists" })
         }
-    
-        return res.send(user)
-};
 
+        return res.json(user)
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+};
 
 
 module.exports = {
